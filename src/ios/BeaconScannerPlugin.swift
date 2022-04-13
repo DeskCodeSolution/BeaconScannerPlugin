19702cede60132eb036ffb4a24964b717c6f5339
nikunj <>
2022-04-13 17:59:05 +0530

    - Timer Add

--- src/ios/BeaconScannerPlugin.swift
+++ src/ios/BeaconScannerPlugin.swift
@@ -2,24 +2,30 @@
 
 @objc(BeaconScannerPlugin) class BeaconScannerPlugin : CDVPlugin{
 
-// MARK: Properties
-var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    // MARK: Properties
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    var timer = Timer()
 
+    @objc(add:) func add(_ command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+        let param1 = (command.arguments[0] as? NSObject)?.value(forKey: "param1") as? Int
+        let param2 = (command.arguments[0] as? NSObject)?.value(forKey: "param2") as? Int
+        if let p1 = param1 , let p2 = param2 {
+            if p1 >= 0 && p1 >= 0{
+                let total = String(p1 + p2)
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: total)
+            }else {
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Please pass valid arguments")
+            }
+        }
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
 
-@objc(add:) func add(_ command: CDVInvokedUrlCommand) {
-var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-let param1 = (command.arguments[0] as? NSObject)?.value(forKey: "param1") as? Int
-let param2 = (command.arguments[0] as? NSObject)?.value(forKey: "param2") as? Int
-if let p1 = param1 , let p2 = param2 {
-if p1 >= 0 && p1 >= 0{
- let total = String(p1 + p2)
-pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: total)
-}else {
-pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Please pass valid arguments")
-}
-}
-self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-}
-
-
+    @objc(timerStart:) func timerStart(_ command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Timer count")
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        })
+    }
 }
--- www/BeaconScannerPlugin.js
+++ www/BeaconScannerPlugin.js
@@ -7,3 +7,7 @@ var exec = require('cordova/exec');
 module.exports.add = function (arg0, success, error) {
     exec(success, error, 'BeaconScannerPlugin', 'add', [arg0]);
 };
+
+module.exports.timerStart = function (success, error) {
+    exec(success, error, 'BeaconScannerPlugin', 'timerStart');
+};
